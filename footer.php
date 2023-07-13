567bb3a289f99a24b8729a80194aa84198672587
GitHolo <ooye2332@gmail.com>
2023-07-13 14:00:49 +0200

    footer fix

--- footer.php
+++ footer.php
@@ -7,12 +7,26 @@
       <a href="mailto:mfelczak@hotmail.com">mfelczak@hotmail.com</a>
       <a href="mailto:borzyskowskib@gmail.com">borzyskowskib@gmail.com</a>
     </div>
-    <?php foreach ($languages as $code => $language) { ?>
-            <a href="?lang=<?php echo $code; ?>">
-                <img src="<?php echo $language['flag']; ?>" alt="<?php echo $language['name']; ?>" width="32" height="24">
-            </a>
-        <?php } ?>
-    <a href="<?php echo $baseUrl; ?>?lang=<?php echo $selectedLanguage?>">
+    <?php
+    $currentURL = $_SERVER['REQUEST_URI'];
+    $newURL = preg_replace('/([&?]lang=[^&]+)/', '', $currentURL);
+    if (strpos($newURL, '?') !== false) {
+        $newURL .= '&lang='.$selectedLanguage;
+    } else {
+        $newURL .= '?lang='.$selectedLanguage;
+    }
+    foreach ($languages as $code => $language) {
+      $langURL = $newURL;
+      if (strpos($newURL, 'lang=') !== false) {
+        $langURL = preg_replace('/([&?]lang=[^&]+)/', '', $newURL);
+      }
+      $langURL .= '&lang='.$code;
+      ?>
+      <a href="<?php echo $langURL; ?>">
+        <img src="<?php echo $language['flag']; ?>" alt="<?php echo $language['name']; ?>" width="32" height="24">
+      </a>
+    <?php } ?>
+    <a href="<?php echo $newURL; ?>">
       <img src="<?php echo $baseUrl; ?>/images/licensed-image.jpg" alt="Logo" class="footer-image" />
     </a>
   </div>
